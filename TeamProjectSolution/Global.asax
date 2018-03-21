409ea49ad4c1526fcfccec3a97620e1b35c45046
snasr <samnasr@live.com>
2018-03-21 14:09:42 -0400

    test

---
+++ TeamProjectSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamProjectSolution.MvcApplication" Language="C#" %>
---
+++ TeamProjectSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamProjectSolution.Startup))]
+namespace TeamProjectSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
